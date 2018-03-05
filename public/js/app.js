533c48719a4f6f48937345cc718e0a8f938de3f3
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-03-04 20:07:35 -0500

    Break long init into smaller functions

--- public/js/app.js
+++ public/js/app.js
@@ -185,51 +185,50 @@ const records = fetchedData.then(response => response.json())
 const lastModified = fetchedData.then(response => new Date(response.headers.get('Last-Modified')))
 const blankRecords = Promise.resolve(Array(100).fill(''))
 
+const searchRecords = (records, query) => {
+	const queryRegexp = new RegExp(query, 'i')
+	return records.filter(r =>
+		r.artist.match(queryRegexp) || r.album.match(queryRegexp))
+}
+
+const handleRouteChange = function () {
+	let lastQuery = ''
+
+	return () => {
+		const query = router.currentRoute()
+		if (query === lastQuery) return
+
+		document.querySelector('#search').value =
+			document.querySelector('#search').value || query
+
+		lastQuery = query
+
+		if (!query) return records.then(resultsComponent.render)
+
+		records
+			.then(records => searchRecords(records, query))
+			.then(resultsComponent.render)
+	}
+}()
+
+const showInitialRecords = records => {
+	if (window.location.hash) {
+		const query = router.currentRoute()
+		document.querySelector('#search').value = query
+		resultsComponent.render(searchRecords(records, query))
+	} else {
+		resultsComponent.render(records)
+	}
+}
+
 function init() {
 	document.body.classList.add('loading')
 	blankRecords.then(resultsComponent.render)
 	records.then(_ => document.body.classList.remove('loading'))
-
-	records.then(records => {
-		if (window.location.hash) {
-			const query = router.currentRoute()
-			document.querySelector('#search').value = query
-			const queryRegexp = new RegExp(query, 'i')
-			const filteredResult = records.filter(r =>
-				r.artist.match(queryRegexp) || r.album.match(queryRegexp))
-
-			resultsComponent.render(filteredResult)
-		} else {
-			resultsComponent.render(records)
-		}
-	})
-
-	records.then(_ => {
-		let lastQuery = ''
-
-		router.onRouteChange(_ => {
-			const query = router.currentRoute()
-			if (query === lastQuery) return
-
-			document.querySelector('#search').value =
-				document.querySelector('#search').value || query
-
-			lastQuery = query
-
-			if (!query) return records.then(resultsComponent.render)
-
-			const queryRegexp = new RegExp(query, 'i')
-
-			records
-				.then(records =>
-					records.filter(r =>
-						r.artist.match(queryRegexp) || r.album.match(queryRegexp)))
-				.then(resultsComponent.render)
-		})
-	})
-
+	records.then(showInitialRecords)
+	records.then(_ => router.onRouteChange(handleRouteChange))
 	records.then(_ => searchComponent.addHandlers().enable().focus())
-	records.then(_=> resultsComponent.addHandlers())
+	records.then(_ => resultsComponent.addHandlers())
 	lastModified.then(_ => lastUpdatedComponent.render())
 }
 // }}}
